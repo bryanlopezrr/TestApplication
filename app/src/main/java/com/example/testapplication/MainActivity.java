b4c7b7c1a1ea1f52dc202f3bba2d34187ef215e1
Bryan Lopez <bryanlopezrr@gmail.com>
2020-01-28 02:08:52 -0500

    first commit with initial setup

---
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.testapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected theLoginButton
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
