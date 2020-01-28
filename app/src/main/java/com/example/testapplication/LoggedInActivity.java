d8fc590aa4b2e14419ae453e3ca0db0298794576
Bryan Lopez <bryanlopezrr@gmail.com>
2020-01-28 02:38:18 -0500

    adding transition to new activity and message

--- app/src/main/java/com/example/testapplication/LoggedInActivity.java
+++ app/src/main/java/com/example/testapplication/LoggedInActivity.java
@@ -3,12 +3,27 @@ package com.example.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoggedInActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        Bundle buffer = getIntent().getExtras();
+
+        String receivedUser = buffer.getString("key");
+
+//        Toast.makeText(LoggedInActivity.this, receivedUser, Toast.LENGTH_LONG).show();
+
+        TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
+        welcomeTextView.setText("Welcome: " + receivedUser);
+        
+
     }
 }
--- app/src/main/java/com/example/testapplication/MainActivity.java
+++ app/src/main/java/com/example/testapplication/MainActivity.java
@@ -2,16 +2,51 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    protected theLoginButton
+    protected Button theLoginButton;
+    protected EditText theUsername;
+    protected EditText thePassword;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initializing
+        theLoginButton = findViewById(R.id.loginButton);
+        theUsername = findViewById(R.id.userNameEditText);
+        thePassword = findViewById(R.id.passwordEditText);
+
+
+        theLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String username = theUsername.getText().toString().trim();
+
+//                Toast.makeText(MainActivity.this, username, Toast.LENGTH_LONG).show();
+
+                opentheLoginActivity(username);
+            }
+        });
+
+    }
+
+    public void opentheLoginActivity(String whoLoggedIn) {
+        Intent transition = new Intent(this, LoggedInActivity.class);
+        transition.putExtra("key", whoLoggedIn);
+        startActivity(transition);
+
+
     }
 }
