54eb2e26cce288b2e0b7560303d4fca6e6fdce36
Bryan Lopez <bryanlopezrr@gmail.com>
2020-02-11 17:30:35 -0500

    new branch with assignment 2

--- app/src/main/java/com/example/testapplication/LoggedInActivity.java
+++ app/src/main/java/com/example/testapplication/LoggedInActivity.java
@@ -2,10 +2,13 @@ package com.example.testapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -55,12 +58,23 @@ public class LoggedInActivity extends AppCompatActivity {
 
         Bundle buffer = getIntent().getExtras();
 
-        String receivedUser = buffer.getString("key");
+        final String receivedUser = buffer.getString("key");
 
 //        Toast.makeText(LoggedInActivity.this, receivedUser, Toast.LENGTH_LONG).show();
 
         TextView welcomeTextView = (TextView) findViewById(R.id.welcomeTextView);
         welcomeTextView.setText("Welcome: " + receivedUser);
+
+        Button theAnotherButton = (Button) findViewById(R.id.anotherButton);
+
+        theAnotherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.example.receiver");
+                intent.putExtra("Comment", receivedUser);
+                startActivity(intent);
+            }
+        });
 
 
     }
